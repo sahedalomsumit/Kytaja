bdfd3d0d76eac18b436cfba24c821171da2ba6f5
Sahed Alom Sumit <87083337+sahedalomsumit@users.noreply.github.com>
2023-10-14 23:35:38 +0300

    v-1.0.1

--- script.js
+++ script.js
@@ -1,121 +1,114 @@
-//navbar scrool effect
-gsap.to(".nav", {
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
+
+gsap.to("#nav", {
   backgroundColor: "#000",
   duration: 0.5,
-  height: "70px",
+  height: "110px",
   scrollTrigger: {
-    trigger: ".nav",
+    trigger: "#nav",
     scroller: "body",
-    //markers:true,
+    // markers:true,
     start: "top -10%",
     end: "top -11%",
     scrub: 1,
   },
 });
 
-//custom cursor
-let body = document.querySelector("body");
-let crsr = document.querySelector(".cursor");
-let crsrBlur = document.querySelector(".cursor-blur");
-
-body.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
-  crsr.style.top = dets.y + "px";
-  crsrBlur.style.left = dets.x - 250 + "px";
-  crsrBlur.style.top = dets.y - 250 + "px";
-});
-
-//black background 2nd section
-gsap.to("#main-sections", {
+gsap.to("#main", {
   backgroundColor: "#000",
   scrollTrigger: {
-    trigger: "#main-sections",
+    trigger: "#main",
     scroller: "body",
+    // markers: true,
     start: "top -25%",
-    end: "top -75%",
-    //markers: true,
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#about-us img,#about-us-in", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from(".card", {
+  scale: 0.8,
+  // opacity:0,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers:false,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
     scrub: 3,
   },
 });
 
-//navbar cursor change
-let navLinks = document.querySelectorAll(".nav-ul li");
-navLinks.forEach(function (elem) {
-  elem.addEventListener("mouseenter", function () {
-    crsr.style.scale = 3;
-    crsr.style.backgroundColor = "transparent";
-    crsr.style.border = "1px solid #fff";
-  });
-  elem.addEventListener("mouseleave", function () {
-    crsr.style.scale = 1;
-    crsr.style.backgroundColor = "#91b508";
-    crsr.style.border = "0px solid #fff";
-  });
-});
-
-//about us stagger
-gsap.from(".about-us-columns", {
-  y: 90,
-  opacity: 0,
-  duration: 1,
-  stagger: 1,
-  scrollTrigger: {
-    trigger: ".about-us-columns",
-    scroller: "body",
-    start: "top 70%",
-    end: "top 55%",
-    //markers: true,
-    scrub: 1,
-  },
-});
-
-gsap.from(".box", {
-  y: 90,
-  opacity: 0,
-  stagger: 1,
-  scrollTrigger: {
-    trigger: ".box",
-    scroller: "body",
-    start: "top 70%",
-    end: "top 55%",
-    //markers: true,
-    scrub: 2,
-  },
-});
-
-gsap.from(".quote-left", {
-  y: -70,
-  x: -70,
-  scrollTrigger: {
-    trigger: "#quote",
-    scroller: "body",
-    scrub: 4,
-    start: "top 60%",
-    end: "top 40%",
-  },
-});
-
-gsap.from(".quote-right", {
-  y: 70,
-  x: 70,
-  scrollTrigger: {
-    trigger: "#quote",
-    scroller: "body",
-    scrub: 4,
-    //markers:true,
-    start: "top 30%",
-    end: "top 10%",
-  },
-});
-
-gsap.to(".heading-waiting", {
-  y: -100,
-  scrollTrigger: {
-    trigger: ".heading-waiting",
-    scroller: "body",
-    scrub: 2,
-    //markers:true,
-    start: "top 70%",
-    end: "top 55%",
-  },
-});
+// Thanks itna aage tak aane ke liye lekin pura code utha ke copy paste karne ki jagah khud ek baar banane ka try karna, kuch naya seekhne ko milega!
